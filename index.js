e5c6868d083d14287b48d4aa58f9415f311dde75
LStefanCoder <you@example.com>
2025-05-16 12:56:15 +0200

    Added bootstrap and ejs modules and experimented with database search functions.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 //in the package.json, a "type": "module" command is necessary to treat all js files as modules
 import {DB} from './dbconnect.js';
+import {search} from './dbconnect.js';
 import express from 'express';
 import bodyParser from 'body-parser';
 
@@ -34,3 +35,50 @@ app.listen(port, (err) => {
     console.log("Listening on " + port.toString());
   
 });
+
+//https://www.geeksforgeeks.org/how-to-make-a-search-function-using-node-express-and-mysql/
+/*app.get('/search', (req, res) => {
+  const searchTerm = req.query.term;
+    if (!searchTerm) {
+        return res.status(400)
+            .json(
+                {
+                    error: 'Search term is required'
+                }
+            );
+    }
+
+    const query = `
+    SELECT * FROM MusicPieces
+    WHERE Title LIKE ?
+  `;
+
+    // Use '%' to perform a partial match
+    const searchValue = `%${searchTerm}%`;
+
+    DB.query(query, [searchValue, searchValue],
+        (err, results) => {
+            if (err) {
+                console
+                    .error('Error executing search query:', err);
+                return res.status(500)
+                    .json(
+                        {
+                            error: 'Internal server error'
+                        });
+            }
+
+            res.json(results);
+        });
+});*/
+
+//https://medium.com/@nicholasstepanov/search-your-server-side-mysql-database-from-node-js-website-400cd68049fa
+app.get('/results', search, (req, res) => {
+  var result = req.result;
+  res.render('pages/results', {
+    results: result.length,
+    term: req.term,
+    result: result,
+    category: req.category
+  });
+})
